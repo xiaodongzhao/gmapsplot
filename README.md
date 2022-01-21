53a458fd21aac9ebbd905db157563fedddc0ab0d
Xiaodong Zhao <xiaodong.zhao@warwick.ac.uk>
2022-01-21 21:42:22 +0000

    add image

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gmapsplot
-A simple script to plot Google Maps satellite image using Matplotlib
+A simple script to download Google Maps satellite image and plot it using Matplotlib with ENU(East-North-Up) coordinates.
+[example](./example.png)
--- example.py
+++ example.py
@@ -7,11 +7,11 @@ import sys
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "gmapsplot"))
 
-center = (52.382882, -1.565237)
+center = (52.382882, -1.565237) # this will be the x,y origin
 zoom = 21
 scale = 1
 gmaps = GMapsPlot(api_key="GOOGLE_MAP_API_KEY", center=center, zoom=18, scale=1, size_meter=(200, 200))
 gmaps.download()
 fig, ax = gmaps.plot()
-ax.scatter([10, 25, 35], [10, 12, 14], s=10)
+ax.scatter([-56, -36, 10, 30, 100], [-42, -30, 10, 30, 100], s=20)
 plt.show()

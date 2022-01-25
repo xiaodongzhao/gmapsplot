c53a75a6f747039a724e0dfcfab6d38e2d4c8a09
Xiaodong Zhao <xiaodong.zhao@warwick.ac.uk>
2022-01-25 09:56:19 +0000

    update example

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # gmapsplot
 A simple script to download Google Maps satellite image and plot it using Matplotlib with ENU(East-North-Up) coordinates.
 
+```python
+import matplotlib.pyplot as plt
+from gmapsplot import GMapsPlot
+
+center = (52.382882, -1.565237) # this will be the x,y origin
+gmaps = GMapsPlot(api_key="GOOGLE_MAP_KEY", center=center, zoom=18, scale=1, size_meter=(200, 200))
+gmaps.download()
+fig, ax = gmaps.plot()
+ax.scatter([-56, -36, 10, 30, 95], [-42, -30, 10, 30, 95], s=20)
+plt.show()
+```
+
 ![example](./example.png)
--- example.py
+++ example.py
@@ -1,17 +1,10 @@
 #! /usr/bin/env python
 import matplotlib.pyplot as plt
 from gmapsplot import GMapsPlot
-import os
-import sys
-
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(cur_dir, "gmapsplot"))
 
 center = (52.382882, -1.565237) # this will be the x,y origin
-zoom = 21
-scale = 1
-gmaps = GMapsPlot(api_key="GOOGLE_MAP_API_KEY", center=center, zoom=18, scale=1, size_meter=(200, 200))
+gmaps = GMapsPlot(api_key="GOOGLE_MAP_KEY", center=center, zoom=18, scale=1, size_meter=(200, 200))
 gmaps.download()
 fig, ax = gmaps.plot()
-ax.scatter([-56, -36, 10, 30, 100], [-42, -30, 10, 30, 100], s=20)
+ax.scatter([-56, -36, 10, 30, 95], [-42, -30, 10, 30, 95], s=20)
 plt.show()
